95a904fef16c3700f1a2d27a4bfc4dc3b2448773
Carlos Perales <carlosperales050@gmail.com>
2023-01-11 20:20:52 -0600

    strict

---
+++ src/scope/strict.js
@@ -0,0 +1,8 @@
+'use strict';   // con esto es necesario tener que declarar y asignar las variables 
+const pi=3.1416;
+console.log(pi);
+
+function myFunction(){
+    return  pi=3.1416;
+}
+console.log(myFunction());
