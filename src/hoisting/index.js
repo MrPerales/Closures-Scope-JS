71297c1684bf91129d7bd00fea84b81b2a6b9e89
Carlos Perales <carlosperales050@gmail.com>
2023-01-16 19:24:53 -0600

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,18 @@
+
+var nameOfDog;// se eleva la variable y como resultado muestra
+console.log(nameOfDog); //undefined
+var nameOfDog='Elmo';
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+var elmo ; //se eleva la variable
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log('el mejor perro es ' + elmo);
+}
+
+var elmo='elmito';
